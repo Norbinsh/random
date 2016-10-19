c4b770a4e50ef1f95f725b32a1bd5e7bff851a1b
Shay E <wowshay@gmail.com>
2016-10-19 10:47:19 +0300

    Updated code

---
+++ nested_json.py
@@ -0,0 +1,32 @@
+json_data = {}
+
+
+# yield from recursion
+def deepiter(x, prefix=''):
+    if isinstance(x, list):
+        for i, v in enumerate(x):
+            yield from deepiter(v, prefix+'[{!r}]'.format(i)) # {!r} = __repr_
+            # formatting
+    elif isinstance(x, dict):
+        for k, v in x.items():
+            yield from deepiter(v, prefix+'[{!r}]'.format(k))
+    else:
+        yield '{} = {!r}'.format(prefix, x)
+
+for s in deepiter(json_data):
+    print(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
