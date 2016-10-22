0751b28ffa74d3988cfccc4b9cfa3998caa41e1a
Shay E <wowshay@gmail.com>
2016-10-23 00:21:57 +0300

    First commit

---
+++ more_generators.py
@@ -0,0 +1,36 @@
+class Reverse(object):
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index -1
+        return self.data[self.index]
+
+
+def main():
+    rev = reversed('My Reversed String')
+    for char in rev:
+        print(char)
+
+if __name__ == '__main__':
+    main()
+
+""" With generators: """
+
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
+
+def main():
+    rev = reverse('My Reversed String')
+    for char in rev:
+        print(char)
+
+if __name__ == '__main__':
+    main()
