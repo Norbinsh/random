7bf0d5d04a2ab549b981e577406917fdb386ec0b
shay elmualem <wowshay@gmail.com>
2017-01-20 00:22:24 +0200

    updated

--- collections_m/chainmap_a.py
+++ collections_m/chainmap_a.py
@@ -1,7 +1,45 @@
 """
-Module dostring first, imports thereafter
+Making use of ChainMap class allowing us to chain together several mappings 
+(dictionaries). Faster than creating new dictionary and update()'ing it, so they say.
 """
 
-# import stuff here
+from collections import ChainMap
+import builtins
+import argparse
+import os
 
 __author__ = "wowshay@gmail.com"
+
+# globals() returns dict of the MODULE namespace
+# locals() returns dict of the CURRENT namespace it resides in (function, for example)
+# vars() returns dict of CURRENT namespace if no arguments (same as locals()),
+# or dict of the argument provided
+# vars(args) == args.__dict__
+
+
+def main():
+    app_defaults = {'username':'admin', 'password':'admin'}
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username')
+    parser.add_argument('-p', '--password')
+    args = parser.parse_args()
+    # print(args.__dict__)
+    command_line_arguments = {key:value for key, value in vars(args).items() if value} 
+    # using vars with argument
+    # here to return a dict type of our existing arguments so we can chain them
+
+
+    chain = ChainMap(command_line_arguments, os.environ, app_defaults) 
+    # Chaining the objects
+    print(chain['username']) 
+    # Now we can access the items from the same chained object.
+
+
+if __name__ == '__main__':
+    main()
+    os.environ['username'] = 'shay'
+    main()
+
+
+
--- collections_m/counter_a.py
+++ collections_m/counter_a.py
@@ -1,7 +1,38 @@
 """
-Module dostring first, imports thereafter
+Counter, dict subclass, can be used in counting objects, elemnts counted are
+stored as dict keys and their count
+value is the dict value.
+
+Some key methods:
+
+'most_common', 'subtract', 'elements'
+
 """
 
-# import stuff here
+from collections import Counter
 
 __author__ = "wowshay@gmail.com"
+
+counter = Counter('superfluous')
+
+print(counter['s'])
+
+print(type(counter))
+
+print(list(counter.elements()))
+print(set(counter.elements()))
+
+print(counter.most_common(2))
+
+counter_one = Counter('encyclopedia')
+counter_two = Counter('pedia')
+print(counter_one)
+counter_one.subtract(counter_two)
+print(counter_one)
+
+
+counter_abc = Counter('aabbccddeeffgghhiijjkkllmmnnoopppppp')
+abc_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
+print("Counter:", counter_abc)
+counter_abc.subtract(abc_list)
+print(counter_abc)
