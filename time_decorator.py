e97a9c05f388732ff38816a428ab63ea6f4f911e
shay e <wowshay@gmail.com>
2017-01-23 23:52:15 +0200

    works

--- time_decorator.py
+++ time_decorator.py
@@ -11,19 +11,22 @@ def time_decorator(function):
         result = function(*args, **kw)
         end = current_time_in_mili()
 
-        print(function.__name__, args[0], kw, end-start)
+        print(function.__name__, args[0], kw, end - start)
         return result
     return timed
+
 
 class Cnt(object):
     @time_decorator
     def poo(self, b=4, c=5):
         time.sleep(0.5)
+
     @time_decorator
     def print_to_x(self, x):
         print(x)
         print([i for i in range(0, x)])
         time.sleep(1)
+
 
 Cnt.print_to_x(Cnt, 50)
 
