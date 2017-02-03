6799aff176098e9d1e721edb5710fb2d909bb65f
shay e <wowshay@gmail.com>
2017-02-03 12:04:14 +0200

    First commit

---
+++ one_liners.sh
@@ -0,0 +1,12 @@
+# List all files in the CWD that contains 'X' in their body.
+# Redirecting stderr and stdout to /dev/null, then echoing the relevant files by names.
+for file in *; do if cat "$file" 2> /dev/null| grep "X" > /dev/null ; then echo "$file"; fi done
+
+# List all files in the CWD whose names starts with 'X.Y'
+# CWD, Files, terminate exec with \; 
+find . -maxdepth 1 -type f -exec basename {} \; | grep ^X.Y
+
+
+
+
+
