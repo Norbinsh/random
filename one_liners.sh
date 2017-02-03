7efd1a39f5bf1bcc7801b32b605d6480ea384a35
shay e <wowshay@gmail.com>
2017-02-03 12:52:52 +0200

    IPV4 Collector

--- one_liners.sh
+++ one_liners.sh
@@ -9,6 +9,10 @@ find . -maxdepth 1 -type f -exec basename {} \; | grep ^X.Y
 # Recursive find on all files starting with "X.Y" and contain "S"
 find . -type f -name "X.Y*" -exec cat {} \; | grep "S" 
 
+# Grab all IP addresses recursively from all files in the CWD,
+# knowing that there's a single IPv4 address per line
+find . -type f -name "access.log*" -exec cat {} \; | grep -oP '(\d{1,3}\.){3}(\d{1,3})'
+
 
 
 
