5545320b315d4c0beac0415964e04694bd815fd4
shay e <wowshay@gmail.com>
2017-02-03 12:38:45 +0200

    Added recursive find one liner

--- one_liners.sh
+++ one_liners.sh
@@ -6,6 +6,9 @@ for file in *; do if cat "$file" 2> /dev/null| grep "X" > /dev/null ; then echo 
 # CWD, Files, terminate exec with \; 
 find . -maxdepth 1 -type f -exec basename {} \; | grep ^X.Y
 
+# Recursive find on all files starting with "X.Y" and contain "S"
+find . -type f -name "X.Y*" -exec cat {} \; | grep "S" 
+
 
 
 
